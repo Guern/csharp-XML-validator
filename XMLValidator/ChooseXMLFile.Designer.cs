d634077501bf70207688f932fa3ecaa89a895520
ma.guernieri <ma.guernieri@reply.it>
2014-09-24 14:52:12 +0200

    First release

---
+++ XMLValidator/ChooseXMLFile.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace XMLValidator
+{
+    partial class ChooseXMLFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChooseXMLFile));
+            this.button1 = new System.Windows.Forms.Button();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFDxml = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.schemaPathTextBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFDschema = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "File XML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(24, 44);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(243, 20);
+            this.filePathTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(57, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose the XML file to be validated";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(91, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(207, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Validate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFDxml
+            // 
+            this.openFDxml.Filter = "XML|*.xml";
+            this.openFDxml.Title = "Choose the XML file to be validated";
+            this.openFDxml.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(74, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Choose the XML Schema file";
+            // 
+            // schemaPathTextBox
+            // 
+            this.schemaPathTextBox.Location = new System.Drawing.Point(24, 112);
+            this.schemaPathTextBox.Name = "schemaPathTextBox";
+            this.schemaPathTextBox.Size = new System.Drawing.Size(243, 20);
+            this.schemaPathTextBox.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(273, 112);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "XML Schema";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFDschema
+            // 
+            this.openFDschema.FileName = "openFDschema";
+            this.openFDschema.Filter = "Schema XSD|*.xsd";
+            this.openFDschema.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk_1);
+            // 
+            // ChooseXMLFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.schemaPathTextBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ChooseXMLFile";
+            this.Text = "XML Validator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFDxml;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox schemaPathTextBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFDschema;
+    }
+}
+
---
+++ XMLValidator/ChooseXMLFile.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml; //Include this name space inorder to use SqlBulkCopy<
+using System.Xml.Schema;
+
+namespace XMLValidator
+{
+    public partial class ChooseXMLFile : Form
+    {
+        String fileName;
+        String schemaName;
+
+        public ChooseXMLFile()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFDxml.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFDschema.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (fileName == null)
+            {
+                MessageBox.Show("Select an XML file!");
+            }
+            else if (schemaName == null)
+            {
+                MessageBox.Show("Select a Schema file!");
+            }
+            else
+            {
+                // validate the XML file
+                if (ValidateXmlWithXsd(fileName, schemaName))
+                {
+                    MessageBox.Show("valid XML!");
+                }
+                else
+                {
+                    MessageBox.Show("Not valid XML");
+                }
+            }
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            fileName = openFDxml.FileName;
+            filePathTextBox.Text = fileName;
+        }
+
+        private void openFileDialog1_FileOk_1(object sender, CancelEventArgs e)
+        {
+            schemaName = openFDxml.FileName;
+            schemaPathTextBox.Text = schemaName;
+        }
+
+        private static bool ValidateXmlWithXsd(string xmlUri, string xsdUri)
+        {
+            try
+            {
+                XmlReaderSettings xmlSettings = new XmlReaderSettings();
+                xmlSettings.Schemas = new System.Xml.Schema.XmlSchemaSet();
+                xmlSettings.ValidationType = ValidationType.Schema;
+                XmlReader reader = XmlReader.Create(xmlUri, xmlSettings);
+
+                // Parse the file.
+                while (reader.Read()) ;
+
+                return true;
+            }
+            catch (System.Xml.XmlException ex)
+            {
+                return false;
+            }
+        }
+            
+    }
+}
